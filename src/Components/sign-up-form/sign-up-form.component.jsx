2341217a7ddfe2dd85c068e2c0c0eab9bb813cb9
Zakroev Andrey <Dr.zakroev@yandex.ru>
2023-11-28 15:58:27 +0300

    🚀

--- src/Components/sign-up-form/sign-up-form.component.jsx
+++ src/Components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
@@ -6,6 +6,8 @@ import {
 
 import FormInput from "../form-input/form-input.components";
 import Button from "../button/button.component";
+
+import { UserContext } from "../../context/user.context";
 
 import "./sign-up-form.styles.scss";
 
@@ -19,6 +21,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+
+  const { setCurrentUser } = useContext(UserContext);
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -37,6 +41,8 @@ const SignUpForm = () => {
         email,
         password
       );
+
+      setCurrentUser(user);
 
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,3 +1,4 @@
+import { async } from "@firebase/util";
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
@@ -6,6 +7,7 @@ import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -76,3 +78,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
   return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => await signOut(auth);
