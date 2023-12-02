6d1de85f34ca4a7caa74a567b12d072b6840b76c
Zakroev Andrey <Dr.zakroev@yandex.ru>
2023-12-02 14:48:56 +0300

    Add items topic

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,16 @@
-import { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import { CategoriesContext } from "../../context/categories.context";
-import CategoryPreview from "../../Components/category-preview/category-preview.component.jsx";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+
 import "./shop.style.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
